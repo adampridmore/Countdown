19544bbe8f34444720f2eb9a67469529324e4818
Adam Pridmore <adam_pridmore@hotmail.com>
2017-03-12 21:53:38 +0000

    - Extracted polish evaluator from script file.

---
+++ Countdown/PolishEvaluator.fs
@@ -0,0 +1,28 @@
+﻿module PolishEvaluator
+
+type Operator = Plus | Minus | Multiply | Divide
+type Item = 
+    | Operator of Operator
+    | Number of int
+
+type Items = 
+    | Item of List<Item>
+
+let evaluate operator a b =
+    match operator with
+    | Plus -> a + b
+    | Minus -> a - b
+    | Multiply -> a * b
+    | Divide -> a / b
+
+let rec evaluateItems items = 
+    match items with
+    | Operator o :: Number(a) :: Number(b) :: [] -> evaluate o a b
+    | Operator o :: Number(a) :: rest -> evaluate o a (evaluateItems rest)
+    | [] -> 0
+    | Number(a) :: [] -> a
+    | Number(_) :: _ -> failwith "Items cannot start with number and more items"
+    | Operator(_) :: Operator(_) :: _ -> failwith "Items cannot start with two operarors"
+    | Operator(_) :: _ -> failwith "Items cannot start with just one operarors"
+
+
--- Countdown/Script1.fsx
+++ Countdown/Script1.fsx
@@ -1,4 +1,7 @@
-﻿let n = [1;2;3]
+﻿#load "PolishEvaluator.fs"
+open PolishEvaluator
+
+let n = [1;2;3]
 
 let apply (a,b) =
     [(a + b);
@@ -7,32 +10,6 @@ let apply (a,b) =
     (a * b)]
 
 apply (1,2)
-
-
-type Operator = Plus | Minus | Multiply | Divide
-type Item = 
-    | Operator of Operator
-    | Number of int
-
-type Items = 
-    | Item of List<Item>
-
-let evaluate operator a b =
-    match operator with
-    | Plus -> a + b
-    | Minus -> a - b
-    | Multiply -> a * b
-    | Divide -> a / b
-
-let rec evaluateItems items = 
-    match items with
-    | Operator o :: Number(a) :: Number(b) :: [] -> evaluate o a b
-    | Operator o :: Number(a) :: rest -> evaluate o a (evaluateItems rest)
-    | [] -> 0
-    | Number(a) :: [] -> a
-    | Number(_) :: _ -> failwith "Items cannot start with number and more items"
-    | Operator(_) :: Operator(_) :: _ -> failwith "Items cannot start with two operarors"
-    | Operator(_) :: _ -> failwith "Items cannot start with just one operarors"
    
 [Operator(Plus);Number(1); Number(2)] |> evaluateItems 
 
@@ -42,5 +19,6 @@ let rec evaluateItems items =
 
 [Number(1)] |> evaluateItems
 
-[Operator(Plus)] |> evaluateItems
+// Invalid
+//[Operator(Plus)] |> evaluateItems
 
