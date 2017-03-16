9a86a1b0f36b79aa5f9c30eed2fa943fbc194a6d
Adam Pridmore <adam_pridmore@hotmail.com>
2017-03-16 21:58:45 +0000

    - Fixed get combinations to start from [0;0] and run to [9;9]

--- Countdown/Script3.fsx
+++ Countdown/Script3.fsx
@@ -1,18 +1,4 @@
-﻿//let numbers = [0;1;2]
-//
-//let unfolder state = 
-//    let mapfolder state item = 
-//        (state, item )
-//    
-//    state 
-//    |> Seq.mapFold mapfolder false
-//
-//    //Some(2,state)
-//
-//Seq.unfold unfolder [0;0;0]
-//|> Seq.take 10
-
-let next max numbers = 
+﻿let next max numbers = 
     let mapfolder (carry: int) (item:int) = 
         match (carry, item) with
         | 1, item when item < (max - 1) -> (item+1, 0)
@@ -30,20 +16,14 @@ let getCombinations length max =
         let nextStateWithCarry = state |> next max
         match nextStateWithCarry with
         | (_,1) -> None
-        | (nextState, _) -> Some(state, nextState)
+        | (nextState, _) -> Some(nextState, nextState)
     
-    Seq.unfold unfolder numbers
+    seq{
+        yield numbers
+        yield! Seq.unfold unfolder numbers
+    }
 
-getCombinations 5 10
+getCombinations 2 10
 |> Seq.map (Seq.toList)
-|> Seq.last
-|> Seq.singleton
+|> Seq.map (List.rev)
 |> Seq.iter (printfn "%A")
-
-
-//let unfolder (state:int seq) = 
-//    Some(state, next max state)
-//    
-//Seq.unfold unfolder (numbers |> List.toSeq)
-//|> Seq.take 20
-//|> Seq.toList
