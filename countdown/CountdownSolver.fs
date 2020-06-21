49fd9302222e473e451faf8268fe7a0d53b6c425
Adam Pridmore <adam_pridmore@hotmail.com>
2020-06-21 17:35:56 +0100

    - Fix whitespace

--- countdown/CountdownSolver.fs
+++ countdown/CountdownSolver.fs
@@ -4,24 +4,24 @@ open StackCalculator2
 open GetCombinations
 
 let mergeOperatorsIntoNumbers numbers (operators : 'T seq) =
-    let operatorsArray = operators |> Seq.toArray
+  let operatorsArray = operators |> Seq.toArray
 
-    numbers
-    |> Seq.mapi(fun i x ->
-        match i with 
-        | 0 -> [x]
-        | i -> [x; (operatorsArray.[i-1]) ] )
-    |> Seq.collect id
+  numbers
+  |> Seq.mapi(fun i x ->
+    match i with 
+    | 0 -> [x]
+    | i -> [x; (operatorsArray.[i-1]) ] )
+  |> Seq.collect id
 
 
 let getTotalsForNumberList (numbers: Item seq) = 
 
-    let executeItemSequence (items: Item seq) = 
-        items |> execute2
+  let executeItemSequence (items: Item seq) = 
+    items |> execute2
 
-    let operators = [Plus;Minus;Multiply;Divide]
-    let numberOfOpertors = (numbers |> Seq.length) - 1
+  let operators = [Plus;Minus;Multiply;Divide]
+  let numberOfOpertors = (numbers |> Seq.length) - 1
 
-    getCombinations2 numberOfOpertors operators
-    |> Seq.map (fun operatorSequence -> mergeOperatorsIntoNumbers numbers operatorSequence)
-    |> Seq.map (fun itemSequence -> (itemSequence |> executeItemSequence, itemSequence) )
+  getCombinations2 numberOfOpertors operators
+  |> Seq.map (fun operatorSequence -> mergeOperatorsIntoNumbers numbers operatorSequence)
+  |> Seq.map (fun itemSequence -> (itemSequence |> executeItemSequence, itemSequence) )
