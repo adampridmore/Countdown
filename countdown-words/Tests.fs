5c50aed50942edf1f4d88662ef7cafe8038a9b7e
Adam Pridmore <adam_pridomre@hotmmail.com>
2022-02-21 21:05:47 +0000

    Letters solver

--- countdown-words/Tests.fs
+++ countdown-words/Tests.fs
@@ -4,85 +4,88 @@ open System
 open System.IO
 open Xunit
 
-[<Fact>]
-let ``My test`` () =
-  Assert.True(true)
-
-let ``Asssrt`` letters word expectedResult Unit = 
-  true
-
-[<Fact>]
-let ``My test 2`` () =
-  let words = File.ReadAllLines("corncob_lowercase.words")
-
-  let letters = "zoning"
-
-  printfn "%d" (words.Length)
-
 let isMatch(letters: String) (word: String) : bool =
   let sortedLetters = letters.ToCharArray() |>  Array.toList |> List.sort
   let wordSorted = word.ToCharArray() |> Array.toList |> List.sort
 
-  // sortedLetters = wordSorted
-
   let rec test(remainingLetters: List<char>) (remainingWord: List<Char>) : bool =
     match (remainingLetters, remainingWord) with
     | (letter::tailLetters), (wordLetter::tailWord) when letter = wordLetter -> test tailLetters tailWord
-    | (letter::_,[]) -> false
+    | (letter::tailLetters), (wordLetter::tailWord) when letter <> wordLetter -> test tailLetters (wordLetter::tailWord)
+    | ([],wordLetter::tail) -> false
     | _,[] -> true
+    | letters, word -> raise (Exception($"Unexpected pattern match: Letters: %A{letters} Word:%A{word}"))
     
   test sortedLetters wordSorted
+
+let LettersAssert letters word expectedResult : Unit =
+  let actualResult : bool = isMatch letters word
+
+  if (actualResult = expectedResult) then
+    ()
+  else
+    let error = $"Expected match: %b{expectedResult} %s{word} to match %s{letters}"
+    raise (Exception(error))
+
+// [<Fact>]
+// let ``test loading file`` () =
+//   let words = File.ReadAllLines("corncob_lowercase.words")
+
+//   printfn "%d" (words.Length)
 
 [<Fact>]
 let ``Is Match When no Match``() =
   let letters = "abc"
   let word = "cheese"
 
-  Assert.False(isMatch letters word)
+  LettersAssert letters word false
 
 [<Fact>]
 let ``Is Match When Exact Match``() =
   let letters = "abc"
   let word = "abc"
-  Assert.True(isMatch letters word)
+
+  LettersAssert letters word true
 
 [<Fact>]
 let ``Is Match When All Letters Exact Match``() =
   let letters = "abc"
   let word = "cba"
-  Assert.True(isMatch letters word)
+  LettersAssert letters word true
 
 [<Fact>]
 let ``Is Match When All Letters Match``() =
   let letters = "abcd"
   let word = "cba"
-  Assert.True(isMatch letters word)
+  LettersAssert letters word true
 
-// [<Fact>]
-// let ``Is Match When All Letters Match with some spare``() =
-//   let letters = "aabc"
-//   let word = "cba"
-//   Assert.True(isMatch letters word)
+[<Fact>]
+let ``Is Match When All Letters Match with some spare``() =
+  let letters = "aabc"
+  let word = "cba"
+  LettersAssert letters word true
 
-// [<Fact>]
-// let ``Is Match When not match``() =
-//   let letters = "a"
-//   let word = "b"
-//   Assert.False(isMatch letters word)
+[<Fact>]
+let ``Is Match When not match``() =
+  let letters = "a"
+  let word = "b"
+  LettersAssert letters word false
 
-// [<Fact>]
-// let ``Real test``() =
-//   let letters = "zorosmofrksw"
-//   let word = "workrooms"
-//   Assert.True(isMatch letters word)
+[<Fact>]
+let ``Real test``() =
+  let letters = "zorosmofrksw"
+  let word = "workrooms"
+  LettersAssert letters word true
 
-// [<Fact>]
-// let ``Search for best match``() =
-//   let letters = "zorosmofrksw"
+[<Fact>]
+let ``Search for best match``() =
+  let letters = "zorosmofrksw"
 
-//   let words = File.ReadAllLines("corncob_lowercase.words")
+  let words = File.ReadAllLines("corncob_lowercase.words")
 
-//   words
-//   |> Seq.filter (fun word = isMatch letters word)
-//   |> Seq.iter (printfn "Answer: $s")
+  words
+  |> Seq.filter (fun word -> isMatch letters word)
+  |> Seq.map( fun word -> (word, word.Length))
+  |> Seq.sortBy(snd)
+  |> Seq.iter(fun (word, length) -> (printfn "Answer: %s (%d)" word length))
 
